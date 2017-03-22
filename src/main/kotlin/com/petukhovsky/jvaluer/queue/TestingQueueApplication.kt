431b196901851045998ae6fa88397cbb6e7eb1ce
petuhovskiy <petuhovskiy@yandex.ru>
2017-03-22 17:21:09 +0300

    spring initializr

---
+++ src/main/kotlin/com/petukhovsky/jvaluer/queue/TestingQueueApplication.kt
@@ -0,0 +1,11 @@
+package com.petukhovsky.jvaluer.queue
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestingQueueApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TestingQueueApplication::class.java, *args)
+}
